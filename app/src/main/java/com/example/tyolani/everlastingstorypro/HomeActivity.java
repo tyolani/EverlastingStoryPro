413f1c8c6d11a21c64ce84c8fd3eb196d48dacbb
Edholm92 <henrik.edholm92@gmail.com>
2017-10-24 13:49:10 +0200

    Updated the books in the carousel

--- app/src/main/java/com/example/tyolani/everlastingstorypro/HomeActivity.java
+++ app/src/main/java/com/example/tyolani/everlastingstorypro/HomeActivity.java
@@ -54,33 +54,15 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         carouselPicker1 = (CarouselPicker)findViewById(R.id.carouselPicker1);
-        //carouselPicker2 = (CarouselPicker)findViewById(R.id.carouselPicker2);
-        //carouselPicker3 = (CarouselPicker)findViewById(R.id.carouselPicker3);
-
-        //Carousel 1 with all images
-        List<CarouselPicker.PickerItem> itemsImages = new ArrayList<>();
-        itemsImages.add(new CarouselPicker.DrawableItem(R.mipmap.kingdom));
-        itemsImages.add(new CarouselPicker.DrawableItem(R.mipmap.ic_launcher_round));
-        itemsImages.add(new CarouselPicker.DrawableItem(R.mipmap.kingdom));
-        CarouselPicker.CarouselViewAdapter imageAdapter = new CarouselPicker.CarouselViewAdapter(this,itemsImages,0);
-        carouselPicker1.setAdapter(imageAdapter);
-
-        //Carousel 2 with all text
-        /*List<CarouselPicker.PickerItem> textItems = new ArrayList<>();
-        textItems.add(new CarouselPicker.TextItem("One",20)); // 20 is text size (sp)
-        textItems.add(new CarouselPicker.TextItem("Two",20)); // 20 is text size (sp)
-        textItems.add(new CarouselPicker.TextItem("Three",20)); // 20 is text size (sp)
-        CarouselPicker.CarouselViewAdapter textAdapter = new CarouselPicker.CarouselViewAdapter(this,textItems,0);
-        carouselPicker2.setAdapter(textAdapter);*/
 
         //Carousel 2 with image and text
-        /*List<CarouselPicker.PickerItem> mixItems = new ArrayList<>();
-        mixItems.add(new CarouselPicker.TextItem("One",20)); // 20 is text size (sp)
-        mixItems.add(new CarouselPicker.DrawableItem(R.mipmap.ic_launcher_round));
-        mixItems.add(new CarouselPicker.TextItem("Three",20)); // 20 is text size (sp)
-        mixItems.add(new CarouselPicker.DrawableItem(R.mipmap.ic_launcher));
+        List<CarouselPicker.PickerItem> mixItems = new ArrayList<>();
+        mixItems.add(new CarouselPicker.DrawableItem(R.drawable.bookcover1));
+        mixItems.add(new CarouselPicker.DrawableItem(R.drawable.bookcover2));
+        mixItems.add(new CarouselPicker.DrawableItem(R.drawable.bookcover3));
+        mixItems.add(new CarouselPicker.DrawableItem(R.drawable.bookcover4));
         CarouselPicker.CarouselViewAdapter mixAdapter = new CarouselPicker.CarouselViewAdapter(this,mixItems,0);
-        carouselPicker3.setAdapter(mixAdapter);*/
+        carouselPicker1.setAdapter(mixAdapter);
     }
 
 
